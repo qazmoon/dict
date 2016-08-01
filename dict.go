1de70e8df2b9b24aa68223c58509ff83ff956684
qazmoon <qazmoon@126.com>
2016-08-02 00:53:27 +0800

    not only one word

--- dict.go
+++ dict.go
@@ -1,106 +1,104 @@
 package main
 
 import (
-"net/http"
-"io/ioutil"
-"strings"
-"fmt"
-"log"
-"golang.org/x/net/html"
-"os"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
 )
 
 func main() {
-	arg_num := len(os.Args)
-
-    if arg_num != 2 {
-    	fmt.Printf("Use : dict  your_word");
-    	return;
-    }
-    word := os.Args[1]
-    httpGet(word)
-    fmt.Println("--------------Ok\n")
+	argNum := len(os.Args)
+	if argNum == 1 {
+		fmt.Printf("Use : dict  your_word")
+		return
+	}
+	var word string
+	for _, a := range os.Args[1:] {
+		word += a
+		word += " "
+	}
+	//word := os.Args[1]
+	httpGet(word)
+	fmt.Println("--------------Ok")
 }
 
 func httpGet(word string) {
-    searchURL := "http://dict.youdao.com/w/" + word + "/#keyfrom=dict2.top"
-    resp, err := http.Get(searchURL)
-    if err != nil {
-        // handle error
-        log.Fatal(err)
-    }
+	searchURL := "http://dict.youdao.com/w/" + word + "/#keyfrom=dict2.top"
+	resp, err := http.Get(searchURL)
+	if err != nil {
+		// handle error
+		log.Fatal(err)
+	}
 
-    defer resp.Body.Close()
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        // handle error
-        log.Fatal(err)
-    }
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+		log.Fatal(err)
+	}
 
-    doc, err := html.Parse(strings.NewReader(string(body)))
-    SearchResult(doc)
+	doc, err := html.Parse(strings.NewReader(string(body)))
+	searchResult(doc)
 }
 
+func searchResult(n *html.Node) {
+	if n.Type == html.ElementNode && n.Data == "div" {
 
-func SearchResult(n *html.Node) {
-    if n.Type == html.ElementNode && n.Data == "div" {
+		findresult := false
+		for _, a := range n.Attr {
+			if a.Key == "id" && a.Val == "phrsListTab" {
+				//fmt.Println(a.Val)
+				findresult = true
+				break
+			}
+		}
 
-        findresult := false;
-        for _, a := range n.Attr {
-            if a.Key == "id" && a.Val == "phrsListTab" {
-                //fmt.Println(a.Val)
+		if findresult {
+			for node := n.FirstChild; node != nil; node = node.NextSibling {
+				hasresult := false
+				if node.Type == html.ElementNode && node.Data == "div" {
+					for _, a := range node.Attr {
+						if a.Key == "class" && a.Val == "trans-container" {
+							//fmt.Println(a.Val)
+							hasresult = true
+							break
+						}
+					}
+				}
 
-                findresult = true;
-                break
-            }
-        }
+				if hasresult {
+					printResult(node)
+					//find over
+					return
+				}
 
-        if findresult {
-            for node := n.FirstChild; node != nil;node = node.NextSibling{
+			}
+		}
+	}
 
-                hasresult := false
-                if node.Type == html.ElementNode && node.Data == "div" {
-                   for _, a := range node.Attr {
-                        if a.Key == "class" && a.Val == "trans-container" {
-                            //fmt.Println(a.Val)
-                            hasresult = true;
-                            break
-                        }
-                    }
-                }
-
-                if hasresult{
-                    PrintResult(node)
-
-                    //find over
-                    return
-                }
-
-            }
-        }
-    }
-
-
-    for c := n.FirstChild; c != nil; c = c.NextSibling {
-        SearchResult(c)
-    }
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		searchResult(c)
+	}
 }
 
-
-func PrintResult(n *html.Node){
-    if n==nil{
+func printResult(n *html.Node) {
+	if n == nil {
 		fmt.Println("hhhhh  nil")
-        return;
-    }
-	
+		return
+	}
 
-    if n.Type == html.ElementNode && n.Data == "li" {
+	if n.Type == html.ElementNode && n.Data == "li" {
 		childnode := n.FirstChild
 		fmt.Println(childnode.Data)
-        PrintResult(n.NextSibling)
-    }
+		printResult(n.NextSibling)
+	}
 
-    for c := n.FirstChild; c != nil; c = c.NextSibling {
-        PrintResult(c)
-    }
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		printResult(c)
+	}
 }
